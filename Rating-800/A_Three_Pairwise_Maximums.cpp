5d82173117a99720371b83b6090031e9b52cef89
Pranav Sharma <ps25372@gmail.com>
2022-03-18 17:45:05 +0530

    Rating 800

---
+++ Rating-800/A_Three_Pairwise_Maximums.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+	cin >> t;
+	while (t--) {
+		vector<int> a(3);
+		for (auto &it : a) cin >> it;
+		sort(a.begin(), a.end());
+		if (a[1] != a[2]) {
+			cout << "NO" << endl;
+		} else {
+			cout << "YES" << endl << a[0] << " " << a[0] << " " << a[2] << endl;
+		}
+	}
+
+    return 0;
+}
