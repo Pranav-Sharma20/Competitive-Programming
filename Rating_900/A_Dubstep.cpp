f54ab96e512bdd846310c69869d6229dcfda9bdf
Pranav Sharma <ps25372@gmail.com>
2022-03-29 17:31:45 +0530

    Rating 900

---
+++ Rating_900/A_Dubstep.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'W' && s[i + 1] == 'U' && s[i + 2] == 'B')
+        {
+            i = i + 2;
+            cout << " ";
+        }
+        else
+        {
+            cout << s[i];
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
+}
