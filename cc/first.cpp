afabbfd5c14c85e17e747025be54431e83502fb3
Pranav Sharma <ps25372@gmail.com>
2022-04-01 15:36:16 +0530

    Rating 900

---
+++ cc/first.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    int n;
+    cin >> n;
+    vector<vector<string>> vec (n, vector<string> (2));
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
