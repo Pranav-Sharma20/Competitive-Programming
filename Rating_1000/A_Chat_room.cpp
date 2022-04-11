bd1a5bd957d1e983e2dfdac0be71da50fe62f5bf
Pranav Sharma <ps25372@gmail.com>
2022-04-11 14:03:03 +0530

    deleted unused file

---
+++ Rating_1000/A_Chat_room.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void solve() {
+    string s;
+    cin >> s;
+    int n = s.size();
+
+    int ind = 0;
+    string ans = "";
+
+
+        
+    if(ans == "hello"){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+         solve();
+    }
+    return 0;
+}
